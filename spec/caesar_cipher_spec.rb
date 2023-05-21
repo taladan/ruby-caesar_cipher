1b766c7419290b6dedbd95acb62ec152dbe122e3
D.J. Crosby <taladan@gmail.com>
2023-05-21 08:37:17 -0400

    fix: remove unnecessary line in test file

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -4,9 +4,7 @@ require_relative '../caesar_cipher'
 
 describe Cipher do
   let(:cipher) { Cipher.new }
-  describe # shift do
   it 'shifts alphabet characters two spaces right' do
-    cipher = Cipher.new
     expect(cipher.shift('Air', 2)).to eql('Ckt')
   end
 end
