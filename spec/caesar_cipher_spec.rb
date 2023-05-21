857b5ea480b969edebad3391a3f5801c65e09bd7
D.J. Crosby <taladan@gmail.com>
2023-05-21 08:44:12 -0400

    fix: add test context

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -5,11 +5,13 @@ require_relative '../caesar_cipher'
 describe Cipher do
   let(:cipher) { Cipher.new }
 
-  it 'shifts alphabet characters two spaces right' do
-    expect(cipher.shift('Air', 2)).to eql('Ckt')
-  end
+  context 'shifts with only alpha characters' do
+    it 'shifts alphabet characters two spaces right' do
+      expect(cipher.shift('Air', 2)).to eql('Ckt')
+    end
 
-  it 'shifts alphabet characters two spaces left' do
-    expect(cipher.shift('Air', -2)).to eql('Ygp')
+    it 'shifts alphabet characters two spaces left' do
+      expect(cipher.shift('Air', -2)).to eql('Ygp')
+    end
   end
 end
